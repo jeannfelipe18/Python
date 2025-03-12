8f54e61df1742760bf26fcce3245bf651a6ef5ce
jeannfelipe18 <135741976+jeannfelipe18@users.noreply.github.com>
2025-03-12 11:13:49 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,60 @@
+#num1=10
+#num2=10
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+#print(num1 // num2)
+
+
+
+#import math
+
+#print(round(2.9)) #faz o arredondamento do numero
+#print(math.pi) #retorna o valor de pi
+#print(abs(-2.9))
+#print(pow(2,3))
+#print(max(2,3))# retorna o valor maximo
+#print(min(2, 3)) #retorna o valor minimo
+#print(sum([1,2,3])) # retorna a soma dos numeros
+#print(chr(65)) # retorna a letra de acordo com o numero
+#print(ord('A')) # retorna o numero de acordo com a letra
+#print(divmod(10, 3))
+#print(math.ceil(2.1))
+#print(math.floor(2.9))
+
+#x = input('Digite um número: ')
+#y = x + 1
+#print(y)
+
+#print(type(x))
+#y = int(x) + 1
+#print(type(y))
+#print(y)
+
+#x = input('Digite um nome: ')
+#print(x)
+
+#print(type(x))
+
+#x = input('Digite um numero1: ')
+#y = input('Digite um numero2: ')
+
+#print(int(x) + int(y))
+
+#print(2 > 1)
+#print(2 >= 1)
+#print(2 < 1)
+#print(2 <= 1)
+#print(2 == 1)
+#print(2 != 1)
+
+meaning = 30 
+           
+#if meaning > 10:
+    #print('Right on!')
+#else:
+    #print('Not today')
+
+#print('All done')
+
+print("Right on!") if meaning > 10 else print("Not today")
