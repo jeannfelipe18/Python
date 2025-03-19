8f42b758e9f9b47b5f779a284da193f2679c2759
jeannfelipe18 <135741976+jeannfelipe18@users.noreply.github.com>
2025-03-19 11:22:05 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,117 @@
+# for i in range(10): # usado para percorrer o numero no numero de posições numeradas
+#    print(i)
+
+#c = 0
+#while c < 10:
+#   print(c)
+#    c += 1
+
+#for i in range(10): # quando chegar no 5 interrompe a contagem
+#    if i == 5:
+#        break
+#    print(i)
+
+#for i in range(10): # faz a contagem e quando chega no 5 ele so pula o numero 5 e continua a contagem
+#    if i == 5:
+#        continue
+#    print(i)
+
+#arr = [1,2,3,4,5,6,7,8,9,10]
+#arr_names = ["Victor", "Zerefos", "Fujita"]
+
+#for i in arr_names:
+#    print(i)
+
+#for i in arr:
+#    print(i)
+
+#and , or, not
+
+#x = 0
+#y = 20
+
+#if x > 0 and y > 0:
+#    print("Ambos x e y são positivos")
+
+#if x > 0 or y > 0:
+#    print("Pelo menos um deles é positivo")
+
+#if not x > 0:
+#    print("x não é positivo")
+
+#my_list = [1,2,3,4,5]
+
+#print(my_list)
+#print(my_list[1])
+#print(my_list[2])
+#print(my_list[3])
+
+#print(my_list[-1])
+#print(my_list[-2])
+#print(my_list[-3])
+
+#print(my_list[0:3])
+#print(my_list[1:4])
+
+#my_list.append(6) # adiciona o numero informado ao final da lista
+#print(my_list)
+
+#my_list.insert(0,0) # insere um elemnto na numeração da ordem que colocar
+#print(my_list)
+
+#my_list.remove(1)
+#print(my_list)
+
+#my_list.clear()
+#print(my_list)
+
+#my_list.reverse()
+#print(my_list)
+
+#my_list.sort() # ordena a lista do maior para o menor
+#print(my_list)
+
+#my_list.count()
+#print(my_list)
+
+#my_list.extend([6,7,8]) # aumenta o tamanho da lista
+#print(my_list)
+
+#print(my_list.index(1))
+
+#Tuple
+#my_tuple = (1,2,3,4,5)
+#print(my_tuple.index(1))
+#print(my_tuple.count(1))
+#print(my_tuple[0])
+#print(my_tuple[-1])
+#print(my_tuple[1:3])
+
+#my_dict = {"name": "John", "age": 30, "city": "New York"}
+#print(my_dict["name"])
+#print(my_dict.get("age"))
+#print(my_dict.keys())
+#print(my_dict.values())
+#print(my_dict.items())
+#my_dict["country"] = "USA"
+#print(my_dict)
+#my_dict.pop("city")
+#print(my_dict)
+
+#def my_function():
+#    print("Hello from a function")
+
+#my_function()
+
+#Função com parametros
+
+#def my_function(param1, param2):
+    print(param1 + param2):
+#my_function("Hello", "world!")
+
+#função com parametros e retorno
+#def my_function(param1, param2):
+#    return param1 + param2
+
+#texto = my_function("Hello", "world")
+#print(texto)
